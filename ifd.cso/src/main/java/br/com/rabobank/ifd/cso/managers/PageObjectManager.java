4612156263d90413c0be000082dd9b5feaa6c6e3
joao.araruna <joao.araruna@RSI1372.rsinet.com.br>
2021-12-17 16:28:07 -0300

    ajustes

--- ifd.cso/src/main/java/br/com/rabobank/ifd/cso/managers/PageObjectManager.java
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/managers/PageObjectManager.java
@@ -2,6 +2,7 @@ package br.com.rabobank.ifd.cso.managers;
 
 import org.openqa.selenium.WebDriver;
 
+import br.com.rabobank.ifd.cso.object.TelaImoveisRuraisObject;
 import br.com.rabobank.ifd.cso.pages.TelaImoveisRuraisPage;
 import br.com.rabobank.ifd.cso.pages.TelaSemoventesPage;
 
@@ -9,18 +10,24 @@ public class PageObjectManager {
 
 	
 	private WebDriver driver;
+	//protected WebDriver driver;
 	private TelaImoveisRuraisPage telaImoveisRuraisPage;
+	private TelaImoveisRuraisObject telaImoveisRuraisObject;
 	private TelaSemoventesPage telaSemoventesPage;
 	
 	public PageObjectManager(WebDriver driver) {
 		this.driver = driver;
 	}
 	
-	public TelaImoveisRuraisPage getTelaImoveisRuraisPage() {
-		return (telaImoveisRuraisPage == null) ? telaImoveisRuraisPage = new TelaImoveisRuraisPage(driver) : telaImoveisRuraisPage;			
-	}
+//	public TelaImoveisRuraisPage getTelaImoveisRuraisPage() {
+//		return (telaImoveisRuraisPage == null) ? telaImoveisRuraisPage = new TelaImoveisRuraisPage(driver) : telaImoveisRuraisPage;			
+//	}
 
 	public TelaSemoventesPage getTelaSemoventePage() {
 		return (telaSemoventesPage == null) ? telaSemoventesPage = new TelaSemoventesPage(driver) : telaSemoventesPage;
+	}
+	
+	public TelaImoveisRuraisObject getTelaImoveisRuraisObject() {
+		return (telaImoveisRuraisObject == null) ? telaImoveisRuraisObject = new TelaImoveisRuraisObject(driver) : telaImoveisRuraisObject;			
 	}
 }
