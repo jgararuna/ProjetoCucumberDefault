a8f13a79216910074863a6fdc6a3e67f706e3531
joao.araruna <joao.araruna@RSI1372.rsinet.com.br>
2022-01-26 16:16:14 -0300

    Inclusao runner imoveis rurais nao proprios

---
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/runners/imoveisrurais/naoproprios/InclusaoRunner.java
@@ -0,0 +1,38 @@
+package br.com.rabobank.ifd.cso.runners.imoveisrurais.naoproprios;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.vimalselvam.cucumber.listener.Reporter;
+
+import br.com.rabobank.ifd.cso.managers.FileReaderManager;
+import br.com.rabobank.ifd.cso.runners.RunnerSetup;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/resources/features/"},
+				 glue = {"br.com.rabobank.ifd.cso.steps.hooks", "br.com.rabobank.ifd.cso.steps.imoveisrurais"},
+				 tags = {"@Inclusao_ImoveisNaoProprios_SemPreencherColunas"},
+                 plugin = {"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:"})
+public class InclusaoRunner extends RunnerSetup{
+	
+	public InclusaoRunner () {
+		super(br.com.rabobank.ifd.cso.runners.imoveisrurais.naoproprios.InclusaoRunner.class);
+	}
+	
+	@BeforeClass
+	public static void setUp() {		
+		new InclusaoRunner();
+	}
+	
+	@AfterClass
+	public static void writeExtentReport() {
+		Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
+	}
+
+
+}
