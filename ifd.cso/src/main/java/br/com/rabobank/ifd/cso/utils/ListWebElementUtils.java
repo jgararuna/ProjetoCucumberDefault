bc0c550240cc9d62f5a914245afe6e6e24ab65ca
joao.araruna <joao.araruna@RSI1372.rsinet.com.br>
2022-01-26 16:28:24 -0300

    listar webelements filhos

---
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/utils/ListWebElementUtils.java
@@ -0,0 +1,32 @@
+package br.com.rabobank.ifd.cso.utils;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ByIdOrName;
+
+public class ListWebElementUtils {
+	
+//	private TestContextUtils testContext;
+//
+//	public ListWebElementUtils(TestContextUtils context) {
+//		testContext = context;
+//	}
+	
+//	private WebDriver driver;
+//
+//	public ListWebElementUtils(WebDriver driver) {
+//		this.driver = driver;
+//	}
+
+	public int listChildOfWebElement(WebElement element) {
+		
+		List<WebElement> elementList = element.findElements(By.xpath("./child::*"));
+		return elementList.size();
+		
+	}
+	
+
+}
