056df6daa81baf3e562a5c9a6eea3d1dbb6da0bb
joao.araruna <joao.araruna@RSI1372.rsinet.com.br>
2022-01-26 16:22:48 -0300

    tela bens moveis page

---
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/pages/TelaBensMoveisPage.java
@@ -0,0 +1,49 @@
+package br.com.rabobank.ifd.cso.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class TelaBensMoveisPage {
+	
+	protected WebDriver driver;
+
+	public TelaBensMoveisPage(WebDriver driver) {
+		this.driver = driver;
+		AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(this.driver, 20);
+		PageFactory.initElements(factory, this);	
+	}
+	
+	@FindBy(xpath = "//*[@id=\"login\"]")
+	protected WebElement cmpLogin;
+
+	@FindBy(xpath = "//*[@id=\"senha\"]")
+	protected WebElement cmpSenha;
+
+	@FindBy(xpath = "//*[@id=\"entrar\"]")
+	protected WebElement btnEntrar;
+
+	@FindBy(xpath = "//*[@id=\"informacoes_patrimoniais\"]")
+	protected WebElement btnInfPatrimoniais;
+
+	@FindBy(xpath = "//*[@id=\"pills-assets-tab\"]")
+	protected WebElement btnAbaBensMoveisImoveis;
+
+	@FindBy(xpath = "//*[@id=\"icone_lixeira\"]")
+	protected WebElement btnLixeira;
+	
+	@FindBy(xpath = "/html/body/modal-container")
+	protected WebElement popup;
+	
+	@FindBy(xpath = "//*[@id=\"deletar\"]")
+	protected WebElement btnDeletar;
+	
+	@FindBy(xpath = "//*[@id=\"pills-assets\"]/app-assets-properties/div/div[2]/app-assets-properties-list/div[2]/table/tbody")
+	protected WebElement listaBensMoveis;
+	
+	@FindBy(xpath = "//*[@id=\"pills-assets\"]/app-assets-properties/div/div[2]/app-assets-properties-list/div[2]/table/tbody/tr[1]/td[1]/div")
+	protected WebElement itemSelecionadoParaExclusao;
+	
+}
