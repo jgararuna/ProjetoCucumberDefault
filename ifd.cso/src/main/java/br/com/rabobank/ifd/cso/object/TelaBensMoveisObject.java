d544b55e6bbc3d313edac9f8a7b221191d5c6e31
joao.araruna <joao.araruna@RSI1372.rsinet.com.br>
2022-01-26 16:23:43 -0300

    tela bens moveis object

---
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/object/TelaBensMoveisObject.java
@@ -0,0 +1,92 @@
+package br.com.rabobank.ifd.cso.object;
+
+
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ByIdOrName;
+
+import br.com.rabobank.ifd.cso.pages.TelaBensMoveisPage;
+import br.com.rabobank.ifd.cso.utils.ListWebElementUtils;
+import br.com.rabobank.ifd.cso.utils.TempoEsperaUtils;
+import junit.framework.AssertionFailedError;
+
+public class TelaBensMoveisObject extends TelaBensMoveisPage{
+	
+	private TempoEsperaUtils espera = new TempoEsperaUtils(driver);
+	private ListWebElementUtils listWebElementUtils = new ListWebElementUtils();
+	int numItensAntes;
+	
+	public TelaBensMoveisObject(WebDriver driver) {
+		super(driver);
+	}
+	
+	// métodos
+
+//	public void logarNoSiteIFD() throws InterruptedException {
+//		
+//		//cmpLogin.click();
+//		cmpLogin.sendKeys("rabobank");
+//		
+//		//cmpSenha.click();
+//		cmpSenha.sendKeys("rabobank1");
+//
+//		btnEntrar.click();
+//
+//	}
+//
+//	public void clicarInformacoesPatrimoniais() {
+//		
+//		espera.verificarElementoExiste(10, btnInfPatrimoniais);
+//		btnInfPatrimoniais.click();
+//		
+//	}
+
+	public void clicarAbaBensMoveisImoveis() throws InterruptedException {
+		
+		espera.verificarElementoClicavel(10, btnAbaBensMoveisImoveis);
+		espera.verificarLoadDesaparecer(10);
+		btnAbaBensMoveisImoveis.click();
+		System.out.println("aba");
+		espera.verificarLoadDesaparecer(10);
+		
+	}
+
+	public void clicarLixeira() throws InterruptedException {
+		
+		
+//		espera.verificarLoadDesaparecer(10);
+		numItensAntes = listWebElementUtils.listChildOfWebElement(listaBensMoveis);
+		System.out.println(numItensAntes);
+		espera.verificarElementoClicavel(10, btnLixeira);
+		//btnLixeira.click();
+
+	}
+	
+	public void clicarDeletar() {
+		
+//		espera.verificarElementoClicavel(10, btnDeletar);
+//		btnDeletar.click();
+//		espera.verificarLoadDesaparecer(10);
+		
+	}
+	
+	public boolean confirmarExclusao() throws InterruptedException {
+		
+		//int numItensDepois = 2;
+		int numItensDepois = listWebElementUtils.listChildOfWebElement(listaBensMoveis);
+		boolean verify = false;
+		if(numItensAntes != numItensDepois) {
+			verify = true;			
+		}
+		
+		return verify;
+
+	}	
+
+}
