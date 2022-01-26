27596c5835126247a6f75e94b5d311d456e90b4e
joao.araruna <joao.araruna@RSI1372.rsinet.com.br>
2022-01-26 16:26:40 -0300

    ajustes screenshots no report e na pasta específica

--- ifd.cso/src/main/java/br/com/rabobank/ifd/cso/utils/ScreenshotUtils.java
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/utils/ScreenshotUtils.java
@@ -11,7 +11,15 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-import com.cucumber.listener.Reporter;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.vimalselvam.cucumber.listener.ExtentCucumberFormatter;
+import com.vimalselvam.cucumber.listener.ExtentProperties;
+import com.vimalselvam.cucumber.listener.Reporter;
+
+import br.com.rabobank.ifd.cso.managers.FileReaderManager;
+
+//import com.cucumber.listener.Reporter;
 
 public class ScreenshotUtils {
 
@@ -22,29 +30,60 @@ public class ScreenshotUtils {
 	}
 
 	public String getScreenshot(String screenshotName) {
- 
+		
+		FolderUtils folderUtils = new FolderUtils();
 		File sourcePath = ((TakesScreenshot) testContext.getWebDriverManager().createDriver())
 				.getScreenshotAs(OutputType.FILE);
 		
 		 String user = System.getProperty("user.dir");
-		 Path path = Paths.get(user + "\\AdvantageShopping\\target\\cucumber-reports\\screenshots");
+		 String mostRecentFolderPath = folderUtils.mostRecentFolder(user + "\\target\\cucumber-reports");
+		 //Path path = Paths.get(user + "\\AdvantageShopping\\target\\cucumber-reports\\output\\screenshots");
+		 
+		 Path path = Paths.get(mostRecentFolderPath +"\\screenshots");
 
 		    try {
 				Files.createDirectories(path);
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-		String caminhoDoArquivo = System.getProperty("user.dir") + "/target/cucumber-reports/screenshots/"
+//		String caminhoDoArquivo = user + "/target/cucumber-reports/output/screenshots/"
+//				+ screenshotName + ".png";
+		
+		String caminhoDoArquivo = mostRecentFolderPath + "/screenshots/"
 				+ screenshotName + ".png";
 
 		File destino = new File(caminhoDoArquivo);
 
 		try {
 			FileUtils.copyFile(sourcePath, destino);
-			Reporter.addScreenCaptureFromPath(caminhoDoArquivo.toString());
+			//Reporter.addScreenCaptureFromPath(caminhoDoArquivo.toString());
+			//com.vimalselvam.cucumber.listener.Reporter.addScreenCaptureFromPath(caminhoDoArquivo.toString());
+			Reporter.addScreenCaptureFromPath(destino.toString());
 		} catch (IOException e) {
 			System.out.println(" O print falhou " + e.getMessage());
-		}
+		} 
+//		catch (NullPointerException e) {
+//			String caminhoDoArquivoReport = mostRecentFolderPath + "/report.html";
+//			File report = new File(caminhoDoArquivoReport);
+//			try {
+//				
+//				ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(caminhoDoArquivoReport);
+//				ExtentReports extent = new ExtentReports();
+//				extent.attachReporter(htmlReporter);
+////				report.createNewFile();
+////				ExtentReports er = new ExtentReporter
+////				er.start();
+////				ExtentProperties extentProperties = ExtentProperties.INSTANCE;
+////				System.out.println("ola " + extentProperties.getReportPath());
+////				extentProperties.setReportPath("target/cucumber-reports/" + mostRecentFolderPath + "/report.html");
+////				System.out.println("ola3" + report.toString());
+//				Reporter.addScreenCaptureFromPath(caminhoDoArquivoReport);
+//			} catch (IOException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+//			//System.out.println(e.getMessage());
+//		}
 
 		return caminhoDoArquivo;
 	}
