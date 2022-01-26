4ce7662c9b652d2cfe65024979851dfede51e787
joao.araruna <joao.araruna@RSI1372.rsinet.com.br>
2022-01-26 16:14:16 -0300

    ajustes

--- ifd.cso/src/main/java/br/com/rabobank/ifd/cso/managers/PageObjectManager.java
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/managers/PageObjectManager.java
@@ -2,8 +2,12 @@ package br.com.rabobank.ifd.cso.managers;
 
 import org.openqa.selenium.WebDriver;
 
+import br.com.rabobank.ifd.cso.object.TelaBensMoveisObject;
+import br.com.rabobank.ifd.cso.object.TelaGeralObject;
 import br.com.rabobank.ifd.cso.object.TelaImoveisRuraisObject;
+import br.com.rabobank.ifd.cso.object.TelaLoginObject;
 import br.com.rabobank.ifd.cso.pages.TelaImoveisRuraisPage;
+import br.com.rabobank.ifd.cso.pages.TelaLoginPage;
 import br.com.rabobank.ifd.cso.pages.TelaSemoventesPage;
 
 public class PageObjectManager {
@@ -11,9 +15,15 @@ public class PageObjectManager {
 	
 	private WebDriver driver;
 	//protected WebDriver driver;
-	private TelaImoveisRuraisPage telaImoveisRuraisPage;
+	//private TelaImoveisRuraisPage telaImoveisRuraisPage;
+	//Métodos comuns
+	private TelaLoginObject telaLoginObject;
+	private TelaGeralObject telaGeralObject;
+	
 	private TelaImoveisRuraisObject telaImoveisRuraisObject;
 	private TelaSemoventesPage telaSemoventesPage;
+	private TelaBensMoveisObject telaBensMoveisObject;
+
 	
 	public PageObjectManager(WebDriver driver) {
 		this.driver = driver;
@@ -22,6 +32,14 @@ public class PageObjectManager {
 //	public TelaImoveisRuraisPage getTelaImoveisRuraisPage() {
 //		return (telaImoveisRuraisPage == null) ? telaImoveisRuraisPage = new TelaImoveisRuraisPage(driver) : telaImoveisRuraisPage;			
 //	}
+	
+	public TelaLoginObject getTelaLoginObject() {
+		return (telaLoginObject == null) ? telaLoginObject = new TelaLoginObject(driver) : telaLoginObject;
+	}
+	
+	public TelaGeralObject getTelaGeralObject() {
+		return (telaGeralObject == null) ? telaGeralObject = new TelaGeralObject(driver) : telaGeralObject;
+	}
 
 	public TelaSemoventesPage getTelaSemoventePage() {
 		return (telaSemoventesPage == null) ? telaSemoventesPage = new TelaSemoventesPage(driver) : telaSemoventesPage;
@@ -29,5 +47,9 @@ public class PageObjectManager {
 	
 	public TelaImoveisRuraisObject getTelaImoveisRuraisObject() {
 		return (telaImoveisRuraisObject == null) ? telaImoveisRuraisObject = new TelaImoveisRuraisObject(driver) : telaImoveisRuraisObject;			
+	}
+	
+	public TelaBensMoveisObject getTelaBensMoveisObject() {
+		return (telaBensMoveisObject == null) ? telaBensMoveisObject = new TelaBensMoveisObject(driver) : telaBensMoveisObject;
 	}
 }
