e012cac7707d2becd44368708682a9e1b2cd923d
joao.araruna <joao.araruna@RSI1372.rsinet.com.br>
2022-01-26 16:22:13 -0300

    tela geral page

---
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/pages/TelaGeralPage.java
@@ -0,0 +1,28 @@
+package br.com.rabobank.ifd.cso.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class TelaGeralPage {
+	
+	protected WebDriver driver;
+	
+	public TelaGeralPage(WebDriver driver) {
+		
+		this.driver = driver;
+		AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(this.driver, 20);
+		PageFactory.initElements(factory, this);
+		
+	}
+	
+	@FindBy(id = "informacoes_patrimoniais")
+	protected WebElement btnInfPatrimoniais;
+	
+	@FindBy(xpath = "/html/body/app-root/app-nav/div[1]")
+	protected WebElement menuLateral;
+	
+
+}
