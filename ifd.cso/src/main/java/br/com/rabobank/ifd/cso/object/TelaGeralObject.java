64afd6645164766bcc108a79c3edce7cbad323a3
joao.araruna <joao.araruna@RSI1372.rsinet.com.br>
2022-01-26 16:24:00 -0300

    tela geral object

---
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/object/TelaGeralObject.java
@@ -0,0 +1,24 @@
+package br.com.rabobank.ifd.cso.object;
+
+import org.openqa.selenium.WebDriver;
+
+import br.com.rabobank.ifd.cso.pages.TelaGeralPage;
+import br.com.rabobank.ifd.cso.utils.TempoEsperaUtils;
+
+public class TelaGeralObject extends TelaGeralPage{
+	
+	private TempoEsperaUtils espera = new TempoEsperaUtils(driver);
+
+	public TelaGeralObject(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void clicarInformacoesPatrimoniais() {
+		
+		espera.verificarElementoExiste(10, btnInfPatrimoniais);
+		btnInfPatrimoniais.click();
+		
+	}
+
+}
