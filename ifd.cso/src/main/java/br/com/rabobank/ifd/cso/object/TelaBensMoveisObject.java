5be1b9e93490529f3102089e6837883a81e3268f
Angra Santos <angra.santos@hotmail.com.br>
2022-02-07 12:33:57 -0300

    Object bens imoveis

--- ifd.cso/src/main/java/br/com/rabobank/ifd/cso/object/TelaBensMoveisObject.java
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/object/TelaBensMoveisObject.java
@@ -1,31 +1,23 @@
 package br.com.rabobank.ifd.cso.object;
 
-
-
-import java.util.List;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ByIdOrName;
 
 import br.com.rabobank.ifd.cso.pages.TelaBensMoveisPage;
 import br.com.rabobank.ifd.cso.utils.ListWebElementUtils;
+import br.com.rabobank.ifd.cso.utils.ScrollUtils;
 import br.com.rabobank.ifd.cso.utils.TempoEsperaUtils;
-import junit.framework.AssertionFailedError;
 
-public class TelaBensMoveisObject extends TelaBensMoveisPage{
-	
+public class TelaBensMoveisObject extends TelaBensMoveisPage {
+
 	private TempoEsperaUtils espera = new TempoEsperaUtils(driver);
 	private ListWebElementUtils listWebElementUtils = new ListWebElementUtils();
+	private ScrollUtils scroll;
 	int numItensAntes;
-	
+
 	public TelaBensMoveisObject(WebDriver driver) {
 		super(driver);
 	}
-	
+
 	// métodos
 
 //	public void logarNoSiteIFD() throws InterruptedException {
@@ -48,68 +40,60 @@ public class TelaBensMoveisObject extends TelaBensMoveisPage{
 //	}
 
 	public void clicarAbaBensMoveisImoveis() throws InterruptedException {
-		
+
 		espera.verificarElementoClicavel(10, btnAbaBensMoveisImoveis);
 		espera.verificarLoadDesaparecer(10);
 		btnAbaBensMoveisImoveis.click();
 		espera.verificarLoadDesaparecer(10);
-		
+
 	}
 
 	public void clicarLixeira() throws InterruptedException {
-		
-		
+
 //		espera.verificarLoadDesaparecer(10);
 		numItensAntes = listWebElementUtils.listChildOfWebElement(listaBensMoveis);
 		espera.verificarElementoClicavel(10, btnLixeira);
-		//btnLixeira.click();
+		// btnLixeira.click();
 
 	}
-	
+
 	public void clicarDeletar() {
-		
+
 //		espera.verificarElementoClicavel(10, btnDeletar);
 //		btnDeletar.click();
 //		espera.verificarLoadDesaparecer(10);
-		
+
 	}
-	
+
 	public boolean confirmarExclusao() throws InterruptedException {
-		
-		//int numItensDepois = 2;
+		// int numItensDepois = 2;
 		int numItensDepois = listWebElementUtils.listChildOfWebElement(listaBensMoveis);
 		boolean verify = false;
-		if(numItensAntes != numItensDepois) {
-			verify = true;			
+		if (numItensAntes != numItensDepois) {
+			verify = true;
 		}
-		
 		return verify;
-
-	}	
-	
-	
-	public void clicarAdicionarBensMoveis() {
-		btnAdicionarBensMoveis.click();
 	}
-	
+
+	public void clicarAdicionarBensMoveis() {
+		espera.verificarElementoClicavel(10, btnAdicionarBemMovel);
+		btnAdicionarBemMovel.click();
+		scroll.scrollElement(cmpDescricaoItem);
+	}
+
 	public void preencherDescricao() {
 		cmpDescricaoItem.click();
 		cmpDescricaoItem.sendKeys("Descrição Teste");
 	}
-	
+
 	public void preencherQuantidade() {
 		cmpQuantidade.click();
 		cmpQuantidade.sendKeys("100");
 	}
-	
+
 	public void preencherValorUnitario() {
 		cmpValorUnitario.click();
 		cmpValorUnitario.sendKeys("100");
 	}
-	
-	
-	
-	
-
 
 }
