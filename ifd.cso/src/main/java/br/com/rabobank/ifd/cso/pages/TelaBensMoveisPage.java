8eb9376c01e832357293e06903d899de0815024b
joao.araruna <joao.araruna@RSI1372.rsinet.com.br>
2022-02-07 12:37:00 -0300

    ajustes

--- ifd.cso/src/main/java/br/com/rabobank/ifd/cso/pages/TelaBensMoveisPage.java
+++ ifd.cso/src/main/java/br/com/rabobank/ifd/cso/pages/TelaBensMoveisPage.java
@@ -30,7 +30,13 @@ public class TelaBensMoveisPage {
 
 	@FindBy(xpath = "//*[@id=\"pills-assets-tab\"]")
 	protected WebElement btnAbaBensMoveisImoveis;
-
+	
+	@FindBy(id = "btn_adicionar_bens_moveis")
+	protected WebElement btnAdd;
+	
+	@FindBy(id = "descrcaoItem")
+	protected WebElement desc;
+	
 	@FindBy(xpath = "//*[@id=\"icone_lixeira\"]")
 	protected WebElement btnLixeira;
 	
@@ -45,5 +51,8 @@ public class TelaBensMoveisPage {
 	
 	@FindBy(xpath = "//*[@id=\"pills-assets\"]/app-assets-properties/div/div[2]/app-assets-properties-list/div[2]/table/tbody/tr[1]/td[1]/div")
 	protected WebElement itemSelecionadoParaExclusao;
+	
+	@FindBy(id = "moveis")
+	protected WebElement btnMoveis;
 	
 }
